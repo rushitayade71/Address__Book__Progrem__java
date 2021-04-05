85b3722fc4fd7382167aabc85140ca945aac61c2
rushitayade71 <rushitayade71@gmail.com>
2021-04-05 08:41:29 +0530

    Added: Adderss Book Info

--- Address_Book_System.java
+++ Address_Book_System.java
@@ -1,3 +1,22 @@
+
+class Contacts{
+	String fName, lName, address, city, state;
+   Long zipCode, phoneNum;
+   String email;
+
+		Contacts(String fName, String lName, String address, String city, String state, long zipCode, long phoneNum, String email){ 
+			this.fName = fName;
+			this.lName = lName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.zipCode = zipCode;
+			this.phoneNum = phoneNum;
+			this.email = email;
+		} 
+}
+
+
 public class Address_Book_System {
 
    public static void main(String args[]) {
